7fc76bf6d4028335d401809fc568d1ca6e7060fc
JWKelly29 <jwkelly29@gmail.com>
2018-04-13 00:58:39 +0100

    added GET todos route

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -16,4 +16,16 @@ module.exports = app => {
       }
     );
   });
+
+  app.get("/todos", (req, res) => {
+    Todo.find().then(
+      todos => {
+        // using obj instead of array in case i want to return more than just todos in future
+        res.send({ todos });
+      },
+      e => {
+        res.status(400).send(e);
+      }
+    );
+  });
 };
