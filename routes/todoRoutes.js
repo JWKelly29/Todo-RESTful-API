5adc8a100754e7601f1f4657e71f7855b32e6f8d
JWKelly29 <jwkelly29@gmail.com>
2018-04-12 20:29:35 +0100

    created routes directory and added post route for todos

---
+++ routes/todoRoutes.js
@@ -0,0 +1,19 @@
+const { mongoose } = require("../db/mongoose");
+const Todo = mongoose.model("Todo");
+
+module.exports = app => {
+  app.post("/todos", (req, res) => {
+    var todo = new Todo({
+      text: req.body.text
+    });
+
+    todo.save().then(
+      doc => {
+        res.send(doc);
+      },
+      err => {
+        res.status(400).send("error: ", err);
+      }
+    );
+  });
+};
--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const { mongoose } = require("./db/mongoose");
 const { User } = require("./models/User");
 const { Todo } = require("./models/Todo");
 
-var app = express();
+const app = express();
+// middleware to be used on incoming request before being sent off to request handlers
+app.use(bodyParser.json());
+
+require("./routes/todoRoutes")(app);
 
 app.listen(3000, () => {
   console.log("starting on point 3000");
 });
+
+module.exports = app;
