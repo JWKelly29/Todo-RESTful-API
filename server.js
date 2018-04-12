e2c92563e9edcf44148892b13ff910ac395a1029
JWKelly29 <jwkelly29@gmail.com>
2018-04-12 19:51:26 +0100

    created express application and installed express, body-parser and prettier

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-var { mongoose } = require("./db/mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { mongoose } = require("./db/mongoose");
+const { User } = require("./models/User");
+const { Todo } = require("./models/Todo");
+
+var app = express();
+
+app.listen(3000, () => {
+  console.log("starting on point 3000");
+});
