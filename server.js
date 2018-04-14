a4ccbb43a26c959b1ac0bef55add7d88606699fd
JWKelly29 <jwkelly29@gmail.com>
2018-04-14 21:50:58 +0100

    refactored server.js, created config file for storing project configuration

--- server.js
+++ server.js
@@ -1,14 +1,4 @@
-var env = process.env.NODE_ENV || "development";
-console.log(`Running in a ${env} environment`);
-
-if (env == "development") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp";
-} else if (env == "test") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest";
-}
-
+require("./config/config");
 const express = require("express");
 const bodyParser = require("body-parser");
 
