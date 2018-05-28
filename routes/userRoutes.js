f193a7d3994bf768d455476bb56a777165b78bec
JWKelly29 <jwkelly29@gmail.com>
2018-05-28 01:03:06 +0100

    added logout route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -41,4 +41,15 @@ module.exports = app => {
         res.status(400).send();
       });
   });
+
+  app.delete("/users/me/token", authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(
+      () => {
+        res.status(200).send();
+      },
+      () => {
+        res.status(400).send();
+      }
+    );
+  });
 };
